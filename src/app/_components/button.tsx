8d662b248068a98d221afd867b8de9641889378a
Damir Modyarov <otomir@yandex.ru>
2023-06-05 17:42:22 +0300

    Migrate UI components to CVA

--- src/app/_components/button.tsx
+++ src/app/_components/button.tsx
@@ -1,16 +1,28 @@
 import { ComponentProps } from 'react'
+import { cva, VariantProps } from 'cva'
+
+const buttonStyles = cva(
+    `text-md flex items-center justify-center gap-2 rounded-lg border px-4 py-2 flex-wrap
+    transition-colors focus:outline-none focus:ring disabled:animate-pulse font-bold`,
+    {
+        variants: {
+            intent: {
+                primary:
+                    'border-red-700 bg-red-500 ring-red-300 hover:bg-red-600 active:bg-red-500 disabled:text-red-400 text-red-50',
+                secondary:
+                    'border-neutral-300 bg-neutral-50 ring-neutral-300 hover:bg-neutral-100 focus:border-neutral-500 active:bg-neutral-50 disabled:text-neutral-400 text-neutral-900',
+            },
+        },
+        defaultVariants: {
+            intent: 'secondary',
+        },
+    }
+)
 
 export default function Button({
     className,
+    intent,
     ...props
-}: ComponentProps<'button'>) {
-    return (
-        <button
-            className={`text-md flex items-center justify-center gap-1 rounded-lg border border-neutral-300
-                bg-neutral-50 px-4 py-2 transition-colors hover:bg-neutral-100 focus:border-neutral-500 focus:outline-none 
-                focus:ring focus:ring-neutral-300 active:bg-neutral-200 disabled:animate-pulse disabled:text-neutral-400
-                ${className}`}
-            {...props}
-        />
-    )
+}: ComponentProps<'button'> & VariantProps<typeof buttonStyles>) {
+    return <button className={buttonStyles({ intent, className })} {...props} />
 }
--- src/app/_components/input.tsx
+++ src/app/_components/input.tsx
@@ -1,13 +1,45 @@
 import { ComponentProps } from 'react'
+import { cva, VariantProps } from 'cva'
+
+const inputStyles = cva('transition-colors focus:outline-none focus:ring', {
+    variants: {
+        type: {
+            text: '',
+            password: '',
+        },
+        intent: {
+            secondary: 'focus:ring-neutral-300',
+        },
+    },
+    compoundVariants: [
+        {
+            type: ['text', 'password'],
+            class: 'rounded-lg border px-4 py-2',
+        },
+        {
+            type: ['text', 'password'],
+            intent: 'secondary',
+            class: 'border-neutral-300 placeholder:text-neutral-300 focus:border-neutral-500',
+        },
+    ],
+    defaultVariants: {
+        type: 'text',
+        intent: 'secondary',
+    },
+})
 
 export default function Input({
+    type,
     className,
     ...props
-}: ComponentProps<'input'>) {
+}: ComponentProps<'input'> & VariantProps<typeof inputStyles>) {
     return (
         <input
-            className={`rounded-lg border border-neutral-300 px-4 py-2 transition-colors placeholder:text-neutral-300
-        focus:border-neutral-500 focus:outline-none focus:ring focus:ring-neutral-300 ${className}`}
+            type={type}
+            className={inputStyles({
+                type,
+                className,
+            })}
             {...props}
         />
     )
