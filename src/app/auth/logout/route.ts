35230b1b8ad3162c7c607710fac684c0d6407a82
Damir Modyarov <otomir@yandex.ru>
2023-06-08 17:05:01 +0300

    Implement logout route

---
+++ src/app/auth/logout/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+import { AUTH_COOKIE_NAME } from '@/auth'
+
+export function GET(request: NextRequest) {
+    cookies().delete(AUTH_COOKIE_NAME)
+    return NextResponse.redirect(new URL('/', request.url))
+}
