5677e3dec5fca860b3fe0b0a2de249c672037668
Damir Modyarov <otomir@yandex.ru>
2023-06-10 14:50:49 +0300

    Update map error icon

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,12 +1,14 @@
 import { getEnvVariable } from '@/util'
 import Main from '@/app/_components/main'
 import ErrorPage from '@/app/_components/error-page'
+import { MapPinOff } from 'lucide-react'
 
 export default function Map() {
     const mapSource = getEnvVariable('MAP_URL')
     if (!mapSource)
         return (
             <ErrorPage
+                icon={MapPinOff}
                 title="Карта не настроена"
                 description="Мы сообщим в наших социальных сетях когда она станет доступна."
                 advancedDescription="Отсутствует переменная окружения MAP_URL."
