2a264d99a0fc5e33c1bdb16ee80d4bd18d2e79d4
Damir Modyarov <otomir@yandex.ru>
2023-06-10 14:41:58 +0300

    Create 404 error page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import ErrorPage from '@/app/_components/error-page'
+import { SearchX } from 'lucide-react'
+
+export default function NotFound() {
+    return (
+        <ErrorPage
+            icon={SearchX}
+            title="Страница не найдена"
+            description="Если вы считаете что так не должно быть, сявжитесь с администрацией."
+            advancedDescription="Путь вызвал функцию `notFound` или не был найден сервером Next.js."
+        />
+    )
+}
