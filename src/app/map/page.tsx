3c414477daab9e03ee34c8c9b4769ed39c4e1b0e
Damir Modyarov <otomir@yandex.ru>
2023-06-24 14:49:21 +0300

    Fix http map rendering

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,9 +1,10 @@
 import { getEnvVariable } from '@/util'
 import Main from '@/app/_components/main'
 import ErrorPage from '@/app/_components/error-page'
-import { MapPinOff } from 'lucide-react'
+import { Info, MapPinOff } from 'lucide-react'
 import { Suspense } from 'react'
 import Profiles from '@/app/map/_components/profiles'
+import Link from 'next/link'
 
 export const metadata = {
     title: 'Карта сервера',
@@ -25,10 +26,25 @@ export default function Map() {
         <Main>
             <iframe
                 className="min-h-screen w-full rounded-lg border border-neutral-300"
-                src={mapSource}
+                src={'/map/embed'}
             >
                 Ору чё у тебя за говно браузер
             </iframe>
+            <div className="mt-2 flex items-center gap-2 text-neutral-600">
+                <Info size={16} />
+                <p>
+                    Если у вас некорректно отображается карта, вы можете открыть
+                    её{' '}
+                    <Link
+                        className="text-red-500"
+                        href={mapSource}
+                        target="_blank"
+                    >
+                        отдельной страницей
+                    </Link>
+                    .
+                </p>
+            </div>
             <Suspense fallback={null}>
                 <Profiles />
             </Suspense>
