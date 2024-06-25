491d0b8d300e6d9d2e033ddef05468bf15d40244
Damir Modyarov <otomir@yandex.ru>
2024-06-25 19:35:21 +0300

    chore: Change Tailwind config to TypeScript

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import { Config } from "tailwindcss"
+
+const config: Config = {
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {},
+    plugins: [],
+}
+
+export default config
